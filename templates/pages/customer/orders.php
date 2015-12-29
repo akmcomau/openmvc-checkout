9206fa738013ba94c2ecd570c45992018987c834
Martin Akman <martin@akm.com.au>
2015-12-29 17:53:27 +1000

    Hide some unused items

--- templates/pages/customer/orders.php
+++ templates/pages/customer/orders.php
@@ -14,23 +14,27 @@
 							<tr>
 								<th nowrap="nowrap"><?php echo $text_reference; ?></th>
 								<th class="hidden-xs" nowrap="nowrap"><?php echo $text_created; ?> <?php echo $pagination->getSortUrls('created'); ?></th>
+								<?php /*
 								<th class="hidden-xs" nowrap="nowrap"><?php echo $text_special_offers; ?> <?php echo $pagination->getSortUrls('special_offers'); ?></th>
 								<th class="hidden-sm hidden-xs" nowrap="nowrap"><?php echo $text_tax; ?> <?php echo $pagination->getSortUrls('tax'); ?></th>
+								*/ ?>
 								<th nowrap="nowrap"><?php echo $text_total; ?> <?php echo $pagination->getSortUrls('total'); ?></th>
 								<th></th>
 							</tr>
 							<?php foreach ($orders as $order) { ?>
 								<?php $this->config->setLocale($order->locale); ?>
-							<tr>
-								<td nowrap="nowrap"><?php echo $order->getReferenceNumber(); ?></td>
-								<td class="hidden-xs"><?php echo $order->created; ?></td>
-								<td class="hidden-xs"><?php echo money_format('%n', $order->special_offers); ?></td>
-								<td class="hidden-sm hidden-xs"><?php echo money_format('%n', $order->tax); ?></td>
-								<td><?php echo money_format('%n', $order->getTotal()); ?></td>
-								<td>
-									<a href="<?php echo $this->url->getUrl('customer/Orders', 'view', [$order->getReferenceNumber()]); ?>" class="btn btn-primary" title="<?php echo $text_view; ?>"><i class="fa fa-edit"></i></a>
-								</td>
-							</tr>
+								<tr>
+									<td nowrap="nowrap"><?php echo $order->getReferenceNumber(); ?></td>
+									<td class="hidden-xs"><?php echo $order->created; ?></td>
+									<?php /*
+									<td class="hidden-xs"><?php echo money_format('%n', $order->special_offers); ?></td>
+									<td class="hidden-sm hidden-xs"><?php echo money_format('%n', $order->tax); ?></td>
+									*/ ?>
+									<td><?php echo money_format('%n', $order->getTotal()); ?></td>
+									<td>
+										<a href="<?php echo $this->url->getUrl('customer/Orders', 'view', [$order->getReferenceNumber()]); ?>" class="btn btn-primary" title="<?php echo $text_view; ?>"><i class="fa fa-edit"></i></a>
+									</td>
+								</tr>
 							<?php } ?>
 						</table>
 					</form>
